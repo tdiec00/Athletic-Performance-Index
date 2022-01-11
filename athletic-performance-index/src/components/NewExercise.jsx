0fcc666426b610731ab22a9cd633af421868da7d
Trung Diec <trungdiec@Trungs-MBP.home>
2022-01-11 10:36:09 -0500

    new exercise component works

--- athletic-performance-index/src/components/NewExercise.jsx
+++ athletic-performance-index/src/components/NewExercise.jsx
@@ -15,29 +15,23 @@ const default_input = {
 const NewExercise = () => {
   const [input, setInput] = useState(default_input);
   const [bodyPart, setBodyPart] = useState("");
+  let api = "";
+  
+  if (bodyPart.bodyPart === "Back") {
+    api = backApi;
+  } else if (bodyPart.bodyPart === "Legs") {
+    api = legsApi;
+  } else if (bodyPart.bodyPart=== "Bicep") {
+    api = bicepsApi;
+  } else if (bodyPart.bodyPart === "Shoulder") {
+    api = shouldersApi;
+  } else if (bodyPart.bodyPart === "Chest") {
+    api = chestApi;
+  } else if (bodyPart.bodyPart === "Tricep") {
+    api = tricepsApi;
+  } else { };
 
-
-  if (bodyPart === "back") {
-    // setBodyPart(backApi)
-    bodyPart = backApi
-  } else if (bodyPart === "legs") {
-    // setBodyPart(legsApi)
-    bodyPart = legsApi
-  } else if (bodyPart=== "biceps") {
-    // setBodyPart(bicepsApi)
-    bodyPart = bicepsApi
-  } else if (bodyPart === "shoulders") {
-    // setBodyPart(shouldersApi)
-    bodyPart = shouldersApi
-  } else if (bodyPart === "chest") {
-    // setBodyPart(chestApi)
-    bodyPart = chestApi
-  } else if (bodyPart === "triceps") {
-    // setBodyPart(tricepsApi)
-    bodyPart = tricepsApi
-  } else {}
-
-  console.log( bodyPart );
+  console.log(bodyPart.bodyPart);
 
   const handleInput = (event) => {
     event.preventDefault();
@@ -60,10 +54,9 @@ const NewExercise = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const fields = input;
-    const res = await bodyPart.post("/", { fields });
+    const res = await api.post("/", { fields });
     setInput(default_input);
   }
-
 
   return (
     <div>
