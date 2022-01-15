9a45ed2423cf46305997c9c60984bfde21fa2eba
Trung Diec <trungdiec@Trungs-MBP.home>
2022-01-14 19:50:48 -0500

    media query update

--- athletic-performance-index/src/components/styles/Top5AndListContainer.styled.js
+++ athletic-performance-index/src/components/styles/Top5AndListContainer.styled.js
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 export const StyledTop5AndListContainer = styled.div`
 display: flex;
+max-width: 1500px;
+margin: auto;
 
 @media (max-width: 850px){
   flex-direction: column;
   align-items: center;
+ 
 }
 `
